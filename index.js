7e40e0fc5b614e9d5a8d7df91e12462623580897
rajkumar <hithisisrajkumar@gmail.com>
2018-04-28 11:55:58 +0530

    First simple bot Done

---
+++ index.js
@@ -0,0 +1,25 @@
+const Readline = require('readline');
+const Chalk = require('chalk');
+const RiveScript = require('rivescript');
+
+const bot = new RiveScript();
+
+const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+bot.loadFile('./Robot.rive', function () {
+    bot.sortReplies();
+    ask();
+}, function (error) {
+    console.log("Error reading file" + error);
+});
+
+function ask() {
+    rl.question('You:', (message) => {
+        var replay = bot.reply('local-user', message);
+        console.log(Chalk.red('Bot:' + replay));
+        ask();
+    });
+}
