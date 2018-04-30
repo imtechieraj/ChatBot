6f723d193188a9d36e1b2759ed8fb7383f337778
rajkumar <hithisisrajkumar@gmail.com>
2018-04-30 16:55:49 +0530

    Code Structure And Slider Intergration on chat bot

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 const BotSpeech = new p5.Speech();
 const socket = io.connect('http://localhost:5000');
-const me = {};
-me.avatar = "https://i.pinimg.com/originals/7e/7a/26/7e7a26d446cc7c623a22c3ec97a170db.png";
-const you = {};
-you.avatar = "https://pbs.twimg.com/profile_images/731022685942218752/oYq9YG3E_400x400.jpg";
+
+//Bot and human DP
+const me = {avatar:"https://i.pinimg.com/originals/7e/7a/26/7e7a26d446cc7c623a22c3ec97a170db.png"};
+const you = {avatar:"https://pbs.twimg.com/profile_images/731022685942218752/oYq9YG3E_400x400.jpg"};
+
+//Date Format Change function-- > Start
 function formatAMPM(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
@@ -14,6 +16,9 @@ function formatAMPM(date) {
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
 }
+//Date Format Change function-- > End
+
+//Reply message Template-- > Start
 function insertChat(who, text, time) {
     if (time === undefined) {
         time = 0;
@@ -48,10 +53,16 @@ function insertChat(who, text, time) {
             $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
         }, time);
 }
+//Reply message Template-- > End
+
+//Template reload-- > Start
 function resetChat() {
     $("ul").empty();
 }
 resetChat();
+//Template reload-- > End
+
+//Message sent Function Trigger - > start
 $(document).ready(function () {
     $(".mytext").on("keyup", function (e) {
         if ((e.keyCode || e.which) == 13) {
@@ -65,6 +76,9 @@ $(document).ready(function () {
     });
 
 });
+//Message sent Function Trigger - > End
+
+//Reply message Trigger function-- > Start
 socket.on('Reply', function (data) {
     var Get_Keyword = data.replay_msg.slice(0, 4);
     if (Get_Keyword === "wiki") {
@@ -74,8 +88,13 @@ socket.on('Reply', function (data) {
         insertChat("me", data.replay_msg, 1500);
     }
 });
+//Reply message Trigger function-- > End
+
+//Wiki API call-- > Start
+
 function WikiCall(url) {
     $.get(url, function (data, status) {
         insertChat("me", data.extract_html, 1500);
     });
 }
+//Wiki API call-- > End
